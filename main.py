9bc8edd33b24f6c749e0813c36d78d88ec76054d
muntasir2001 <muntasirenamalhuda@gmail.com>
2021-07-17 12:16:04 +0100

    made gui class

--- main.py
+++ main.py
@@ -8,28 +8,32 @@ class MyWindow(QMainWindow):
 	def __init__(self):
 		## calling the constructor of QMainWindow
 		super(MyWindow, self).__init__()
+		
+		self.setGeometry(200, 200, 1000, 500) # xpos, ypos, width, height # xpos and ypos says where to show up the window on your screen
+		self.setWindowTitle("Dashboard")
 		self.initUI()
 		
 	def initUI(self):
 		# label
-		label = QtWidgets.QLabel(self) # initialing label variable
-		label.setText("First label") # setting label text
-		label.move(50, 50) # move the label (x, y)
+		self.label = QtWidgets.QLabel(self) # initialing label variable
+		self.label.setText("First label") # setting label text
+		self.label.move(50, 50) # move the label (x, y)
 		
 		# button
-		b1 = QtWidgets.QPushButton(self) # creating a button
-		b1.setText("Click me") # setting the text
-		b1.clicked.connect(clicked) # event listener for the button
+		self.b1 = QtWidgets.QPushButton(self) # creating a button
+		self.b1.setText("Click me") # setting the text
+		self.b1.clicked.connect(self.clicked) # event listener for the button
 
 	# event listener for the button
 	def clicked(self):
 		self.label.setText("button pressed")
+		
+		
 
 def window():
 	app = QApplication(sys.argv) # starter of the GUI
-	win = QMainWindow() # making a window
-	win.setGeometry(200, 200, 1000, 500) # xpos, ypos, width, height # xpos and ypos says where to show up the window on your screen
-	win.setWindowTitle("Dashboard")
+	win = MyWindow() # making a window
+	
 	
 	
 	
